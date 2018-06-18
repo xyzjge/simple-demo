e3a6b520a19000037cfc97952666bc049861c633
XYZJGE <xyzjge@gmail.com>
2018-06-18 18:33:37 -0300

    Fullscreen

--- src/main/java/ar/com/xyz/simpledemo/XYZDemoMainGameLoop.java
+++ src/main/java/ar/com/xyz/simpledemo/XYZDemoMainGameLoop.java
@@ -7,8 +7,14 @@ import ar.com.xyz.simpledemo.gamestate.SimpleDemoMenuGameState;
 
 public class XYZDemoMainGameLoop extends AbstractMainGameLoop {
 
+//	private static final int WIDTH = 800 ;
+//	private static final int HEIGHT = 600 ;
+	
+	private static final int WIDTH = 1280 ;
+	private static final int HEIGHT = 720 ;
+	
 	public XYZDemoMainGameLoop(String title) {
-		super(title, 800, 600, true) ;
+		super(title, WIDTH, HEIGHT, true) ;
 	}
 
 	public static void main(String[] args) {
